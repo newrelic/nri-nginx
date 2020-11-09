ade3c1c46efb83f129bbde2778f67cc96e0ea659
Grigorii Merkushev <brushknight@gmail.com>
2020-11-09 12:32:07 +0100

    ci: fix config issues in msi package

--- build/windows/fix_archives.sh
+++ build/windows/fix_archives.sh
@@ -27,7 +27,7 @@ for zip_dirty in $(find dist -regex ".*_dirty\.\(zip\)");do
   echo "===> Move files inside ${zip_file_name}"
   mv ${ZIP_CONTENT_PATH}/nri-${INTEGRATION}.exe "${AGENT_DIR_IN_ZIP_PATH}/bin"
   mv ${ZIP_CONTENT_PATH}/${INTEGRATION}-win-definition.yml "${AGENT_DIR_IN_ZIP_PATH}"
-  mv ${ZIP_CONTENT_PATH}/${INTEGRATION}-win-config.yml.sample "${CONF_IN_ZIP_PATH}"
+  mv ${ZIP_CONTENT_PATH}/${INTEGRATION}-config.yml.sample "${CONF_IN_ZIP_PATH}"
 
   echo "===> Creating zip ${ZIP_CLEAN}"
   cd "${ZIP_CONTENT_PATH}"
