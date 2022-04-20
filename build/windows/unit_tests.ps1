638d72df92c4252fb95cb3da35c2d960068d57cd
Guillermo Sanchez Gavier <gsanchez@newrelic.com>
2022-04-20 16:23:24 +0200

    fix windows unit tests script

--- build/windows/unit_tests.ps1
+++ build/windows/unit_tests.ps1
@@ -1,11 +1,6 @@
-echo "===> Install unit tests dependencies"
-
-go get github.com/axw/gocov/gocov github.com/AlekSi/gocov-xml
-go get -v -d -t ./...
-
 echo "===> Running unit tests"
 
-gocov test ./src/...
+go test ./src/...
 if (-not $?)
 {
     echo "Failed running tests"
