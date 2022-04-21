53af90f3fa3a01599ce6be916410ca4b55275fb3
Guillermo Sanchez Gavier <gsanchez@newrelic.com>
2022-04-21 08:18:34 +0200

    🧹 Move to Go 1.18 and bump dependencies (#89)

--- build/ci.mk
+++ build/ci.mk
@@ -35,16 +35,6 @@ ci/test: ci/deps
 			-w /go/src/github.com/newrelic/nri-$(INTEGRATION) \
 			$(BUILDER_TAG) make test
 
-.PHONY : ci/snyk-test
-ci/snyk-test:
-	@docker run --rm -t \
-			--name "nri-$(INTEGRATION)-snyk-test" \
-			-v $(CURDIR):/go/src/github.com/newrelic/nri-$(INTEGRATION) \
-			-w /go/src/github.com/newrelic/nri-$(INTEGRATION) \
-			-e SNYK_TOKEN \
-			-e GO111MODULE=auto \
-			snyk/snyk:golang snyk test --severity-threshold=high
-
 .PHONY : ci/build
 ci/build: ci/deps
 ifdef TAG
